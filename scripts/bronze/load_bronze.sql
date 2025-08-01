e06c6efb1671f64f0021e159813948596dfdad26
Arie Trisnasaputra <123148978+ArieTrisna@users.noreply.github.com>
2025-08-01 18:03:58 +0800

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -1,3 +1,23 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+Database: PostgreSQL
+
+Usage Example:
+    call bronze.load_bronze(); <- PostgreSQL
+===============================================================================
+*/
+
 create or replace procedure bronze.load_bronze()
 language plpgsql
 as $$
